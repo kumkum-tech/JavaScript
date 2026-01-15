6d6c9d80ca7d6770faf778821d28823d4e504d92
Kumkum Rastogi <rastogikumkum42@gmail.com>
2026-01-16 00:31:32 +0530

    java script basics

---
+++ Dates.js
@@ -0,0 +1,7 @@
+//  DATES>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let mydate =new Date();
+// let myCreatedDate = new Date.now()
+
+// console.log(myTimeStamp);
+// console.log(Date.now())';
---
+++ Maths.js
@@ -0,0 +1,17 @@
+// ===================MATHSS+++++++=+++++++++++++++++++++++++++++++
+
+/*console.log(Math);
+console.log(Math.abs(-4));//Math./abs => representing the absolute value:::
+console.log(Math.abs(4)); // 
+console.log(Math.ceil(4.2));// thoda sa bhi value bada hoga to max value le lenga...
+console.log(Math.min(4,5,2,6));
+console.log(Math.max(4,5,6,8));
+console.log(Math.round(7.4));
+console.log(Math.floor(7.4)); // lowest value lega ......*/
+// console.log(Math.random());//value in 0 and 1..
+// console.log(Math.random()*10 +1);//ye value o dega
+// console.log(Math.random()*10 +1);
+
+// const min =10
+// const max =20
+// console.log(Math.floor(Math.random() *(max-min + 1))+ +min)
