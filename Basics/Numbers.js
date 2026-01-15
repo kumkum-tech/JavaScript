76be520f236332bf2c29e987a07d2e01887778c6
Kumkum Rastogi <rastogikumkum42@gmail.com>
2026-01-16 01:17:52 +0530

    Add files via upload

---
+++ Basics/Numbers.js
@@ -0,0 +1,14 @@
+            ///// NUMBERS  #######################
+// const score=400
+// console.log(score)
+// const balance =new Number(100)
+// console.log(balance);
+// console.log (balance.toString)
+// console.log(balance.toFixed(2));// ye precision value ke liye use hota hai .....
+// const otherNumber =23.456
+
+// console.log(otherNumber.toPrecision(3));// ye value ko round off karke deta haiii...
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));//value me commmas laga deta hai....(ye indian value me deta hai sab kuch .....)
+
+// en in=> for indian,,,,,,or<<,,()normal for ...
