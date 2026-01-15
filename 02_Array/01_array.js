4519b1eee62b7d7f835fc27518ee6ac171b7b039
Kumkum Rastogi <rastogikumkum42@gmail.com>
2026-01-16 01:23:46 +0530

    Add files via upload

---
+++ 02_Array/01_array.js
@@ -0,0 +1,23 @@
+// // ARRAY
+
+//  const myArr =[ 0,1,2,3,4,5];
+//  const myArr3 = [1,2,3,4,5,6,7];
+// const myHeros = ["muskan","sakshi"]
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[0]);
+
+// //array methods:::
+// // myArr.push(6)
+// // myArr.push(7)
+// // myArr.push(8)
+// // myArr.pop()//pop will be remove the last value of the element
+// // myArr.unshift(9)
+// // myArr.shift()
+// // console.log(myArr.includes(12));
+// // console.log(myArr.indexOf(3));
+// // const newArr = myArr.join()//used to bind our ARRAY 
+
+// // console.log(myArr);
+// // console.log( typeof newArr);
+// // console.log( newArr);
+// // console.log(myArr);
---
+++ 02_Array/02_method.js
@@ -0,0 +1,11 @@
+// //  slice and spice.........
+
+console.log("A",myArr);
+
+const myN = myArr.slice(1,3);
+console.log(myN);
+console.log("B",myArr);
+
+const myN2 = myArr.splice(1,3);
+console.log(myN2);
+console.log("C", myArr);
