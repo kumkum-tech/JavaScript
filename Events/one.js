0867fe0f63d7275bbf26aed7fe76c4a32bfe0796
Kumkum Rastogi <rastogikumkum42@gmail.com>
2026-03-18 01:16:52 +0530

    Events

---
+++ Events/one.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html events</title>
+</head>
+<body style="background-color: #414141;color:aliceblue">
+    <h2>amazing images </h2>
+    <div>
+        <ul id ="images">
+            <li> <img width="200px"
+                 id= "ocean"
+                 src=" https://images.pexels.com/photos/34125813/pexels-photo
+                 -34125813.jpeg" alt="phptoshop">
+            </li>
+            <li> <img width="200px"
+                 id= "phptoshop"
+                 src= "https://images.pexels.com/photos/34020185/pexels-photo-34020185.jpeg" alt ="animal">
+</li>
+<li> <img width="200px"
+                 id= "flower"
+                 src="https://images.pexels.com/photos/34036546/pexels-photo-34036546.jpeg" alt = "flower">
+    <li><img width="200px"
+                 id= "garden"
+                 src=" https://images.pexels.com/photos/17489114/pexels-photo-17489114.jpeg" alt="senery">
+                   </li>
+                   <li <a> href="sssssss"></li>
+        </ul>
+    </div>
+    
+</body>
+<script>
+    document.getElementById('owl').addEventListener('click',function(){alert("owl clicked again")},)
+    //attachEvent()
+    // jQuery - 
+</script>
+</html>
